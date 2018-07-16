1405a0aee1583d8891f3be6f5bd074f30294a0ce
mgso <mashuai@birdcc.com>
2018-07-16 15:18:18 +0800

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+const uglify = require('uglifyjs-webpack-plugin');
 module.exports = {
-  entry: './lib/index.js',
-  output: {
-    filename: './index.min.js'
-  }
+    entry: './index.js',
+    output: {
+        filename: '../dist/index.min.js',
+        libraryTarget: "umd",
+        library: "imgZip",
+        umdNamedDefine: true
+    },
+    optimization: {
+        minimizer: [
+            new uglify({
+                uglifyOptions: {
+                    compress: false
+                }
+            })
+        ]
+    },
 };
